7cf33e63f1c24f2fbfc4f7bc307809b3422366ab
UponTheSky <sailing.to.orbis@gmail.com>
2025-08-06 01:58:37 +0900

    docs-intermediate-testing: finished

--- intermediate/advanced-testing/calculate.go
+++ intermediate/advanced-testing/calculate.go
@@ -1,7 +1,17 @@
 package calculate
 
-func Sum(numbers []int) int {
+func SumInt(numbers []int) int {
 	total := 0
+
+	for _, n := range numbers {
+		total += n
+	}
+
+	return total
+}
+
+func SumFloat64(numbers []float64) float64 {
+	total := 0.0
 
 	for _, n := range numbers {
 		total += n
